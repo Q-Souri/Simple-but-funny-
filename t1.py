108b183e7df560935e36254169baf66cfbe2a911
Q. Souri <79165967+Q-Souri@users.noreply.github.com>
2021-12-26 01:15:51 +0000

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,16 @@
+number1 = int(input("Please enter first number: "))
+number2 = int(input("Please enter thr seconf number: "))
+number3 = int(input("Please enter the third number: " ))
+
+largest_number = number1
+if number2 > largest_number:
+    largest_number = number2
+if number3 > largest_number:
+    largest_number = number3
+print("The largest number is: ", largest_number)
+
+    
+                    
+
+
+
